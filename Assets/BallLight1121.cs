5f6589926735b2c29d30c89b1643ebef7599bdb0
Jiaxin(Ellie) Huang <jh6003@nyu.edu>
2022-11-21 18:08:32 -0500

    new1121

---
+++ Assets/BallLight1121.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLight1121 : MonoBehaviour
+{
+    [SerializeField] private GameObject tr;
+
+    //[SerializeField] private float speed = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.up = (CapsuleBag.transform.position - transform.position) * -1;
+
+
+        transform.LookAt(tr.transform);
+    }
+}
